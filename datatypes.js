ecf5a4c2e0680c9940f0cfe69e39654498b7dced
vivianritah <143530424+vivianritah@users.noreply.github.com>
2024-02-23 13:37:45 +0300

    Functions

---
+++ datatypes.js
@@ -0,0 +1,8 @@
+let x= 16 + "Volvo";
+console.log(x);
+
+let y = "Volvo" +16 +4 ;
+console.log(x);
+
+let z= 16 + 4 + "harrier"
+console.log(z);
---
+++ function.js
@@ -0,0 +1,18 @@
+function product(p1,p2){
+    return p1*p2
+}
+const myProduct=product(5,8)
+console.log(myProduct)
+
+
+function difference(n1,n2){
+    return n2-n1
+}
+console.log (difference(10, 50))
+console.log (difference(8, 9))
+
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+console.log(toCelsius(77))
+// document.getElementById("demo").innerHTML = toCelsius(77)
